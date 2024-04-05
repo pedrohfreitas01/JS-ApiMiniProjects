151d81fddc2136ebc2358fefb4ccab68f81be164
pedrohfc <pedrofreitas_ph@hotmail.com>
2024-04-05 12:57:34 -0300

    randomJoke

--- randomJoke/script.js
+++ randomJoke/script.js
@@ -3,9 +3,13 @@ const btn = document.getElementById("btn")
 const url = "https://v2.jokeapi.dev/joke/Miscellaneous,Dark,Pun,Spooky,Christmas?lang=en&blacklistFlags=racist,sexist&type=single";
 
 let getJoke = () => {
+    jokeContaier.classList.remove('fade')
     fetch(url)
         .then(data => data.json())
-        .then(item => jokeContaier.textContent = `${item.joke}`)
+        .then(item => {
+            jokeContaier.textContent = `${item.joke}`;
+            jokeContaier.classList.add("fade")
+        })
 }
 
 btn.addEventListener('click', getJoke)
