1c5470cb1d67dc3f061d78a5a5af196120747596
pedrohfc <pedrofreitas_ph@hotmail.com>
2024-04-30 21:16:05 -0300

    weatherApp Done

---
+++ weatherApp/script.js
@@ -0,0 +1,64 @@
+
+let result = document.getElementById("result")
+let searchBtn = document.getElementById("src-button")
+let cityRef = document.getElementById("city")
+let apiKey = "c5caaac70a98087b0c1bc85a3dd81f18"
+
+
+
+let getWeather = () =>{
+    let cityValue = cityRef.value
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`;
+    
+    cityRef.value = ""
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            let dataWeather = data.weather[0]
+            let dataMain = data.main
+
+            console.log(data);
+            
+
+            result.innerHTML = `
+            
+            <h2>${cityValue}</h2>
+            <h4 class="weather">${dataWeather.main}</h4>
+            <h4 class="descri">${dataWeather.description}</h4>
+            <img src="https://openweathermap.org/img/w/${dataWeather.icon}.png" alt="">
+            <h1>${dataMain.temp} &#176;</h1>
+            
+            <div class="temp-container">
+                <div>
+                    <h4 class="title">max</h4>
+                    <h4 class="temp">${dataMain.temp_max}</h4>
+                </div>
+                <div>
+                    <h4 class="title">min</h4>
+                    <h4 class="temp">${dataMain.temp_min}</h4>
+                </div>
+            </div>
+            `;
+        })
+        //if city name is NOT valid
+        .catch(() => {
+            result.innerHTML = `<h3 class="msg">City not found</h3>`
+        })
+
+
+    }
+
+
+searchBtn.addEventListener("click" , getWeather)
+
+
+
+
+
+
+
+/* 
+
+
+
+*/
